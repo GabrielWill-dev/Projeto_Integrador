7c8ba05ed7d1afaccfd516b56b41ab604227780f
G_Will <gabrielwillsilva@gmail.com>
2024-09-28 09:37:35 -0300

    Update registrar.php

--- IFRIEND/frontend/register/registrar.php
+++ IFRIEND/frontend/register/registrar.php
@@ -15,7 +15,7 @@
         <div class="row vh-100 align-items-center justify-content-center">
             <div class="col-10 col-sm-8 col-md-6 col-lg-4 p-4 bg-white shadow rounded">
                 <div class="row justify-content-center mb-4">
-                    <img src="../../img/logo_agendador.png" alt="Agendador">
+                    <img src="../../img/iFriendatt.png" alt="Agendador">
                 </div>
                 <form class="needs-validation" action="../../backend/register/registrar.php" method="post" novalidate>
                     <div class="form-group mb-4">
